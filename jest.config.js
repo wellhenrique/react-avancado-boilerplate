a3520a97c9416f7c3f4ca7b8c843647653d3972b
wellhenrique <welissonh80@gmail.com>
2022-05-22 13:49:17 -0300

    feat: adding initial styles <Main />

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,10 @@ module.exports = {
   collectCoverageFrom: ["src/**/*.ts(x)?", "!src/**/stories.tsx"],
   setupFilesAfterEnv: ["<rootDir>/.jest/setup.ts"],
   modulePaths: ["<rootDir>/src/"],
+  moduleNameMapper: {
+    "styled-components":
+      "<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js"
+  },
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }]
   }
